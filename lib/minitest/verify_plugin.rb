59d67e1369aae25981a8585f8a73ff7907a9ffec
Thomas Marshall <thomas@thomasmarshall.com>
2024-09-14 20:51:32 +0100

    Improve option description

--- lib/minitest/verify_plugin.rb
+++ lib/minitest/verify_plugin.rb
@@ -2,7 +2,7 @@ require "minitest/verify"
 
 module Minitest
   def self.plugin_verify_options(opts, options)
-    opts.on "--verify", "Verify tests sometimes fail." do
+    opts.on "--verify", "Verify tests are not false negatives after running them." do
       options[:enabled] = true
     end
   end
