f171262d9595149cf120c4a0a21b6408873080c3
Thomas Marshall <thomas@thomasmarshall.com>
2024-09-24 20:56:20 +0100

    Fix option name

--- lib/minitest/verify_plugin.rb
+++ lib/minitest/verify_plugin.rb
@@ -3,11 +3,11 @@ require "minitest/verify"
 module Minitest
   def self.plugin_verify_options(opts, options)
     opts.on "--verify", "Verify tests are not false negatives after running them." do
-      options[:enabled] = true
+      options[:verify] = true
     end
   end
 
   def self.plugin_verify_init(options)
-    Verify.enabled = options.fetch(:enabled, false)
+    Verify.enabled = options.fetch(:verify, false)
   end
 end
